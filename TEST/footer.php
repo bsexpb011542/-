3f14b02bb746f768e7e8d32c43ca2e1f3035e446
bsexpb011542 <42913937+bsexpb011542@users.noreply.github.com>
2025-07-04 00:10:30 +0800

    Delete TEST directory

--- TEST/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="text-center bg-light py-4">
-    <h3>東海水果公司</h3>
-    <p>電郵 : abcdefg@example.com &nbsp;&nbsp; 電話：02-8888-8888</p>
-    <p>&copy; <?php echo date('Y')?> 版權所有 不得轉載</p>
-</footer>
-</body>
-</html>
--- TEST/head2.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'dbConnect.php';
-?>
-<li><a href="index.php">登出</a></li>
-<li><a href="mema01.php">查詢水果</a></li>
-<li><a href="mema02.php">下訂單</a></li>
-<li><a href="mema03.php">查詢我的訂單</a></li>
-<li><a href="mema04.php">刪除訂單</a></li>
-<li><a href="mema05.php">修改基本資料</a></li>
